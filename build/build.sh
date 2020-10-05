44585345114b68a8b0e8f27f183457cb6441af57
Vincent Dechenaux <vincent.dechenaux@gmail.com>
2020-10-05 22:18:50 +0200

    Statically build the binary

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,6 @@ cd /tmp
 wget https://github.com/google/brotli/archive/v$VERSION.zip
 unzip v$VERSION.zip
 cd brotli-$VERSION
-LDFLAGS=-m32 CFLAGS=-m32 make -j $(nproc) brotli
+LDFLAGS="-m32 -static" CFLAGS="-m32 -static" make -j $(nproc) brotli
 strip bin/brotli
 cp bin/brotli /output/brotli-i386
